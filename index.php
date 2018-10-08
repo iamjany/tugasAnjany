5c46683a08c2ae078076e365f3ffc478dd4b0c1c
Anjany <anjanyrisqiati@gmail.com>
2018-10-08 08:44:43 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Biografi Anjany Risqiati</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/stylish-portfolio.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    
+  
+
+    <!-- Header -->
+    <header class="masthead d-flex">
+      <div class="container text-center my-auto">
+        <h1 class="mb-1">BIOGRAFI </h1>
+        <h3 class="mb-5">
+          <em>Anjany Risqiati</em>
+        </h3>
+        <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">ABOUT ME</a>
+      </div>
+      <div class="overlay"></div>
+    </header>
+
+    <!-- About -->
+    <section class="content-section bg-light" id="about">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h2>BIODATA ANJANY RISQIATI</h2>
+            <p class="lead mb-5">Saya adalah seorang Mahasiswa di UNIVERSITAS ISLAM NEGRI SUNAN GUNUNG DJATI BANDUNG. <br>
+              Saya berada dijurusan Teknik Informatika. Saya berasal dari Sukabumi.<br>
+            Saya senang membaca novel dan menulis cerpen.</p>
+            </p>
+            <a class="btn btn-dark btn-xl js-scroll-trigger" href="#services">Resume Biodata</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Services -->
+    <section class="content-section bg-primary text-white text-center" id="services">
+      <div class="container">
+        <div class="content-section-heading">
+          <h3 class="text-secondary mb-0">Here I AM</h3>
+          <h2 class="mb-5">DATA DIRI</h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-screen-smartphone"></i>
+            </span>
+            <h4>
+              <strong>NAMA </strong>
+            </h4>
+            <p class="text-faded mb-0">ANJANY RISQIATI</p>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class=""></i>
+            </span>
+            <h4>
+              <strong>TTL</strong>
+            </h4>
+            <p class="text-faded mb-0">SUKABUMI 22 MEI 1998.</p>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+            <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class=""></i>
+            </span>
+            <h4>
+              <strong>HOBI</strong>
+            </h4>
+            <p class="text-faded mb-0">MEMBACA NOVEL
+             
+             !</p>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class=""></i>
+            </span>
+            <h4>
+              <strong>MOTO HIDUP</strong>
+            </h4>
+            <p class="text-faded mb-0">YOU CAN IF YOU BELIEVE YOU CAN </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Callout -->
+   
+  </body>
+
+</html>
